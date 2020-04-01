b2a208e723f02402fa2017092fad321957e319a9
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-03-31 23:54:52 -0400

    update before using context

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import LoginForm from './components/auth/LoginForm';
 import Loader from './/loader/Loader'
 import Loader2 from './components/messageBoard/components/loader/Loader'
 import axios from 'axios'
+import AUTH_SERVICE from './services/auth/AuthServices'
 
 import './App.css';
 
@@ -30,47 +31,69 @@ export class App extends Component {
     };
   }
 
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  displayError = (err) => {
+    if(err.response.data.message){
+      this.setState({ isLoading: true,message: err.response.data.message})
+     console.log(err.response.data.message)
+   }else{
+     console.log(err)
+   }
+  }
+
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   isUserLoggedIn = async () => {
     try {
       this.setState({isLoading: true, message: 'Checking authentication...'})
-      const res = await axios.get('/api/auth/isLoggedIn');
+      const res = await AUTH_SERVICE.isLoggedIn();
+      
+      // const res = await axios.get('/api/auth/isLoggedIn');
       this.setState({user: res.data.user })
-      const timer = Math.floor(Math.random()* 3000) + 1000
-      // console.log("Output for: App -> isUserLoggedIn -> res", res)
+      
       setTimeout(()=> {
         this.setState({ loggedIn: true})
         this.getUsers()
       }, 1000)
       setTimeout(()=> {
         this.setState({isLoading: false})
-      },timer)
+      },1500)
     } catch (err) {
-       if(err.response.status === 401){
-        console.log(err.response.data.message)
-      }else{
-        console.log(err)
-      }
+       this.displayError(err)
       setTimeout(()=> {
         this.setState({isLoading: false})
-      },1500)
+      },2000)
     }
   }
 
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   getUsers = async () => {
-    this.setState({isLoading: true})
-    let res = await axios.get(`/api/auth/users`)
-    // console.log("getUsers -> res", res)
+    try {
+      this.setState({isLoading: true})
+    let res = await AUTH_SERVICE.getUsers()
+    // let res = await axios.get(`/api/auth/users`)
     this.setState({ users: res.data, isLoading: false})
+    } catch (err) {
+      this.displayError(err)
+    }
   }
 
   handleSignup = async e => {
     e.preventDefault()
-    this.setState({isLoading: true, message: 'Signing in...'})
-    const res = await axios.post('/api/auth/signup', this.state.userSignup[0]);
-    this.setState({ user: res.data.user })
+    try {
+      this.setState({isLoading: true, message: 'Checking authentication...'})
+      const res = await AUTH_SERVICE.signup(this.state.userSignup[0]);
+      // const res = await axios.post('/api/auth/signup', this.state.userSignup[0]);
+      this.setState({ message: 'Signing in...'})
+      this.setState({ user: res.data.user })
     setTimeout(()=> {
       this.setState({ loggedIn: true, isLoading: false})
     },2000)
+    } catch (err) {
+      this.displayError(err)
+    }
   }
 
   handleChange = e => {
@@ -84,17 +107,23 @@ export class App extends Component {
   }
   userLogout = async () => {
     this.setState({isLoading: true, message: 'Logging out...'})
-    await axios.post('/api/auth/logout');
-    this.setState({ loggedIn: false, user: null })
+    await AUTH_SERVICE.logout();
+    // await axios.post('/api/auth/logout');
+    this.setState({ loggedIn: false, user: null, users: null })
+    setTimeout(() => {
+      this.setState({isLoading: true, message: 'Successfully logged out!'})
+    }, 1500)
     setTimeout(() => {
       this.setState({ isLoading: false})
-    }, 2000)
+    }, 2500)
   }
   componentDidMount() {
-    console.log('componentDidMount')
-    this.isUserLoggedIn()
+    this.setState({isLoading: true, message: 'Checking authentication...'})
+    setTimeout(() => {
+      this.isUserLoggedIn()
+    },1000)
   }
-  updateState = () => {
+  userLoggedIn = () => {
     this.isUserLoggedIn()
   }
   
@@ -119,21 +148,24 @@ export class App extends Component {
               }}
             />
             <Route exact strict path="/login" render={props => {
-            return  loggedIn ? <Redirect to='/' /> : <LoginForm {...props} updateState={this.updateState} />
+            return  loggedIn ? <Redirect to='/' /> : <LoginForm {...props} userLoggedIn={this.userLoggedIn} />
             }} />
             <Route exact strict path="/about" component={About} />
             <Route exact strict path="/user-update" component={UserForm} />
             <Route exact strict path="/projects" component={Projects} />
             <Route exact strict path="/projects/:id" component={ProjectDetails} />
+            {/* <Route exact strict path="/message-board" component={SocketMessageBoard} />
+            <Route exact strict path="/message-board/:id" component={SocketMessageBoard} /> */}
+
               <Route
               exact strict
               path="/message-board"
-              render={props =>  <SocketMessageBoard {...props} user={user} users={users}/> }
+              render={props => loggedIn? <SocketMessageBoard {...props} user={user} users={users} /> : ''}
             />
             <Route
               exact strict
               path="/message-board/:id"
-              render={props =>  <SocketMessageBoard {...props} /> }
+              render={props => users? <SocketMessageBoard {...props} /> : <Redirect to='/message-board'/>}
             />
             {/* <Route exact strict path="/projects/:id" component={ProjectDetails} />
               <Route
