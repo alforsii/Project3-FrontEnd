04c5c3c5ef5e4a8962986cf0a530ed7b0dd95d70
Cule219 <culaficstefan@gmail.com>
2020-04-02 14:38:52 -0400

    routes

--- src/services/messagesAuth/MessagesAuth.js
+++ src/services/messagesAuth/MessagesAuth.js
@@ -1,18 +1,16 @@
-import axios from 'axios'
-const BASE_URL = process.env.REACT_APP_BASE_URL
+import axios from "axios";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const service = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true
-})
-
+  baseURL: BASE_URL,
+  withCredentials: true
+});
 
 export const AUTH_MESSAGES = {
-    // updateStatus: data => service.post('/api/messages/update-status'),
-    getUserBoards:  () =>  service.get('/api/messages/boards'),
-    updateUserBoard:  data =>  service.post('/api/messages/board', data),
-    addNewMessage:  data =>  service.post('/api/messages/add-new-message', data),
+  // updateStatus: data => service.post('/api/messages/update-status'),
+  getUserBoards: () => service.get("/api/messages/boards"),
+  updateUserBoard: data => service.post("/api/messages/board", data),
+  addNewMessage: data => service.post("/api/messages/add-new-message", data)
+};
 
-}
-
-export default AUTH_MESSAGES
+export default AUTH_MESSAGES;
